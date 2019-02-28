196a5e2bfc93cf08ea5f0b6c52e0afa6fb038a70
Ariel <ariel@Ariels-MacBook-Pro-2.local>
2019-02-28 12:46:10 -0600

    xxx

---
+++ README.md
@@ -0,0 +1,49 @@
+# Recon-Scripts
+
+## arinscript 
+arinscript takes a company name e.g. Google and outputs Net Range, CIDR, and Customer/Organization name for the child and parent networks. 
+
+### Running arinscript
+Currently, ARIN only supports the terminal command line. To run the script, simply type the scriptname and the company name. 
+
+*./arinscript Google*
+
+ARIN utilizes wildcard queries so substitute an asterisk for the alphanueric character that you want to leave off at the beginning of the query term. For example if you want The Google Company, type
+
+*./arinscript \*Google\**
+
+For more details, visit https://www.arin.net/resources/services/whois_guide.html
+
+Note: arinscript has not been extensively tested. Since a lot of commands are based on the exact format of the limited tests I have run, some wonky output may appear. 
+
+## shodanscript.py
+shodanscript.py takes a list of IP addresses and creates a csv file with the shodan output. 
+
+This script is modified from https://scooby.blog/2018/01/11/shodan-search-multiple-ip-addresses-with-python-script/
+
+### Running shodanscript.py
+To get shodanscript running, you must have a) python installed b) shodan installed
+
+
+To install shodan on Windows, download python then follow these steps
+
+*py -m easy_install shodan* 
+
+
+Most likely, you will get a lot of errors saying packages are missing. A few packages that might be missing are: urllib3, chardet, certifi, idna, and click
+*py -m easy_install packageThatIsMissing*
+
+
+Log on to your Shodan account and copy your API key on the top right bar
+
+*py -m shodan init yourAPIkey*
+
+
+To **finally** run the package
+
+*py .\shodanscript.py .\iplist.txt*
+
+// iplist.txt is the list of ip in a text file
+
+
+
---
+++ shodanScript.py
@@ -0,0 +1,86 @@
+# modified from url 
+# https://scooby.blog/2018/01/11/shodan-search-multiple-ip-addresses-with-python-script/
+from sys import argv, exit
+import time
+
+try:
+    import shodan
+    from shodan.cli.helpers import get_api_key
+except ImportError:
+    print("Please install the shodan libary")
+    exit()
+
+if len(argv) != 2:
+    print ("Usage: python shodanScript.py iplist.txt")
+    exit()
+
+api_key = get_api_key()
+api = shodan.Shodan(api_key)
+
+input_file = argv[1]
+
+#open csv file
+output_csv = open("shodanOutput.csv", "w+")
+
+#write header line 
+output_csv.write("IP,Last_shodan_update,ORG,ISP,OS,Product,Country,City, Open_port(s)\n")
+
+#function to iterate through ip addresses
+def banaan(ip_address):
+    try:
+        host = api.host(ip_address)
+
+        foundIP = "%s,%s,%s,%s,%s,%s,%s,%s," % (
+            host.get('ip_str'),
+            host.get('last_update' ,'n/a'),
+            host.get('org' ,'n/a'),
+            host.get('isp' ,'n/a'),
+            host.get('os' ,'n/a'),
+            host.get('product' ,'n/a'),
+            host.get('country_name' ,'n/a'),
+            host.get('city' ,'n/a')
+        )
+
+        output_csv.write(foundIP)
+
+        #write first port
+        output_csv.write(str(host['data'][0]['port']))
+        
+        #write / if more than one port exists
+        for x in host['data'][1:]:
+            output_csv.write(";" + str(x['port']))
+        output_csv.write('\n')
+
+    #print none if no results pop up    
+    except Exception as e:
+        output_csv.write("%s, %s,,,,,,,,,\n" % (ip_address, str(e)))
+        print(e)
+
+#open list of ip addresses
+f = open(input_file ,'r')
+
+#print to console searching for ip addresses
+for x in f:
+    ip = x.strip()
+    # output_file.write(colored('\n' + '[*] Searching for ' + ip, 'green'))
+    print('[*] Searching for ' + ip)
+    banaan(ip)
+    time.sleep(1)
+
+#close the output file 
+output_csv.close()
+
+# to install shodan on windows
+#py -m easy_install shodan 
+# will get a bunch of errors saying a lot of packages aren't install
+# py -m easy_install packageThatIsMissing
+
+#py -m shodan init yourAPIkey
+# yourAPIkey can be found on shodan.io after you make an account
+
+#python .\shodanScript.py .\iplist.txt
+#will output to shodanOutput.csv 
+
+# other computers write
+# py .\shodanScript.py .\iplist.txt
+
