1e2b3391564be280589aca5db5e755a91adbe5e0
Ariel <ariel@Ariels-MacBook-Pro-2.local>
2019-03-20 16:30:32 -0500

    changed names, deleted out-of-date files

--- shodanScraper.py
+++ shodanScraper.py
@@ -11,19 +11,20 @@ except ImportError:
     exit()
 
 if len(argv) != 2:
-    print ("Usage: python shodanScript.py iplist.txt")
+    print ("Usage: python list_test.py ")
     exit()
 
 api_key = get_api_key()
 api = shodan.Shodan(api_key)
 
 input_file = argv[1]
+file_name = input_file + "-shodanoutput.csv"
 
 #open csv file
-output_csv = open("shodanOutput.csv", "w+")
+output_csv = open(file_name, "w+")
 
 #write header line 
-output_csv.write("IP,Last_shodan_update,ORG,ISP,OS,Product,Country,City, Open_port(s)\n")
+output_csv.write("IP,Last_shodan_update,ORG,ISP,OS,Product,Country,City, Open_port(s), Port_header\n")
 
 #function to iterate through ip addresses
 def banaan(ip_address):
@@ -33,28 +34,32 @@ def banaan(ip_address):
         foundIP = "%s,%s,%s,%s,%s,%s,%s,%s," % (
             host.get('ip_str'),
             host.get('last_update' ,'n/a'),
-            host.get('org' ,'n/a'),
-            host.get('isp' ,'n/a'),
+            host.get('org' ,'n/a').replace(","," "),
+            host.get('isp' ,'n/a').replace(","," "),
             host.get('os' ,'n/a'),
             host.get('product' ,'n/a'),
             host.get('country_name' ,'n/a'),
             host.get('city' ,'n/a')
         )
 
+
+
         output_csv.write(foundIP)
 
         #write first port
         output_csv.write(str(host['data'][0]['port']))
-        
+        output_csv.write("," + "\"" + str(host['data'][0]['data']).replace("\r\n", ";").replace(",","").replace("\"", "").strip() + "\"" + "\n" )
+
         #write / if more than one port exists
         for x in host['data'][1:]:
-            output_csv.write(";" + str(x['port']))
-        output_csv.write('\n')
+            output_csv.write(foundIP)
+            output_csv.write(str(x['port']))
+            output_csv.write("," + "\"" + str(x['data']).replace("\r\n", ";").replace(",","").replace("\"", "").strip() + "\"" + "\n")
 
     #print none if no results pop up    
     except Exception as e:
-        output_csv.write("%s, %s,,,,,,,,,\n" % (ip_address, str(e)))
-        print(e)
+        output_csv.write("%s, %s,,,,,,,\n" % (ip_address, str(e)))
+
 
 #open list of ip addresses
 f = open(input_file ,'r')
@@ -69,6 +74,7 @@ for x in f:
 
 #close the output file 
 output_csv.close()
+print('[*][*][*] Ouput stored as ' + file_name)
 
 # to install shodan on windows
 #py -m easy_install shodan 
@@ -83,4 +89,3 @@ output_csv.close()
 
 # other computers write
 # py .\shodanScript.py .\iplist.txt
-
